61b8d30bc542636b6f8df5e92e72c409466fb085
tanya <tedien2016@yandex.ru>
2026-08-26 01:17:28 +0400

    селектор выбора показателя для создания формы добавления результатов

--- src/components/AddMultiResultsModal.vue
+++ src/components/AddMultiResultsModal.vue
@@ -2,8 +2,17 @@
   <AppModal
     ref="add-multi-results-modal"
     title="Добавить несколько результатов"
+    @on-close="clearData"
   >
     <div style="min-width: 480px;">
+      <AppFormField
+        label="Выберите показатель"
+        type="select"
+        :model-value="null"
+        :select-list="allowedParams"
+        @update:model-value="createForm($event)"
+      />
+      
       <!-- Управление формой -->
       <div class="mt-3 flex">
         <AppBtn
@@ -22,7 +31,10 @@
 
 <script setup>
 import AppModal from '@/components/shared/AppModal'
-import { useTemplateRef } from 'vue'
+import AppFormField from '@/components/shared/inputs/AppFormField'
+import { computed, ref, useTemplateRef } from 'vue'
+import { useTestStore } from '@/stores/testStore.js'
+import { storeToRefs } from 'pinia'
 
 const addMultiResultsModalRef = useTemplateRef('add-multi-results-modal')
 
@@ -34,5 +46,21 @@ function open() {
 
 function saveData () {
   addMultiResultsModalRef.value.close()
+}
+
+const { arrListData } = storeToRefs(useTestStore())
+const forms = ref([])
+const allowedParams = computed(() => {
+  return arrListData.value
+      .map(({ title, id }) => ({ label: title, value: id }))
+      .filter(({ value }) => !forms.value.includes(value))
+})
+
+function createForm(paramId) {
+  forms.value.push(paramId)
+}
+
+function clearData () {
+  forms.value = []
 }
 </script>
