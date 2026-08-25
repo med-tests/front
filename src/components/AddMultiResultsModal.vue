599664aec3ab29a86fd900605ea235928b3502e3
tanya <tedien2016@yandex.ru>
2026-08-26 01:07:19 +0400

    создать и подключить модалку добавления нескольких результатов

---
+++ src/components/AddMultiResultsModal.vue
@@ -0,0 +1,38 @@
+<template>
+  <AppModal
+    ref="add-multi-results-modal"
+    title="Добавить несколько результатов"
+  >
+    <div style="min-width: 480px;">
+      <!-- Управление формой -->
+      <div class="mt-3 flex">
+        <AppBtn
+          class="ml-auto"
+          type="success"
+          @click="saveData"
+        >
+          <div class="px-2">
+            Сохранить
+          </div>
+        </AppBtn>
+      </div>
+    </div>
+  </AppModal>
+</template>
+
+<script setup>
+import AppModal from '@/components/shared/AppModal'
+import { useTemplateRef } from 'vue'
+
+const addMultiResultsModalRef = useTemplateRef('add-multi-results-modal')
+
+defineExpose({ open })
+
+function open() {
+  addMultiResultsModalRef.value.show()
+}
+
+function saveData () {
+  addMultiResultsModalRef.value.close()
+}
+</script>
--- src/components/MainContextMenu.vue
+++ src/components/MainContextMenu.vue
@@ -19,6 +19,10 @@
       </div>
     </template>
   </AppContextMenu>
+
+  <AddMultiResultsModal
+    ref="add-multi-results-modal"
+  />
 </template>
 
 <script setup>
@@ -26,6 +30,8 @@ import AppContextMenu from '@/components/shared/AppContextMenu'
 import PlusIcon from '@/components/icons/PlusIcon'
 import { storeToRefs } from 'pinia'
 import { useApiStore } from '@/stores/apiStore.js'
+import AddMultiResultsModal from '@/components/AddMultiResultsModal.vue'
+import { useTemplateRef } from 'vue'
 
 const emit = defineEmits(['openCreateModal'])
 
@@ -33,12 +39,17 @@ const { loading } = storeToRefs(useApiStore())
 
 const contextMenuList = [
   { title: 'Создать показатель', event: 'createParameter' },
+  { title: 'Добавить несколько результатов', event: 'addMultiResults' },
 ]
 
+const addMultiResultsModalRef = useTemplateRef('add-multi-results-modal')
 function onContextMenuClick (eventName) {
   switch (eventName) {
     case 'createParameter':
       emit('openCreateModal')
+      break
+    case 'addMultiResults':
+      addMultiResultsModalRef.value.open()
       break
     default:
       console.log('Не указан обработчик для действия: ', eventName)
